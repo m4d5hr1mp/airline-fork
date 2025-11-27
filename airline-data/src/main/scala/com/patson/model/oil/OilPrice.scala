c8904f1aafbdb37293f25c3dd2b35dbe4956164e
M4d5hr1mp <egorkhristov00@gmail.com>
2025-11-27 13:07:11 +0100

    Up-Adjusting Fuel Costs globally

--- airline-data/src/main/scala/com/patson/model/oil/OilPrice.scala
+++ airline-data/src/main/scala/com/patson/model/oil/OilPrice.scala
@@ -1,9 +1,10 @@
 package com.patson.model.oil
 
 case class OilPrice(price: Double, cycle: Int)
-
+// Price used before deploying to playtest = 70 <-> 0.684, margins of 40-50% at default prices, too high.
+// Price used during playtest = 70 <-> 0.725
 object OilPrice {
-  val DEFAULT_UNIT_COST = 0.684 //This is cost per 1 KG of Jet-A! Not per Liter! (And at 70$ Oil price exactly)
+  val DEFAULT_UNIT_COST = 0.725 //This is cost per 1 KG of Jet-A! Not per Liter! (And at 70$ Oil price exactly)
   val DEFAULT_PRICE: Double = 70
   // the price used for actual simulation calculation
   val unitCost: (Double => Double) = (price: Double) => price / DEFAULT_PRICE * DEFAULT_UNIT_COST
